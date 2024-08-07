3a553856cb4c0f1238b97997a3187ffc161629b2
Ravi <m.v.raviteja@outlook.com>
2024-08-07 23:09:38 +0530

    first commit

---
+++ src/main/java/com/example/CurdProjMangoDB/CurdProjMangoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.CurdProjMangoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdProjMangoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdProjMangoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CurdProjMangoDB/CurdProjMangoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CurdProjMangoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdProjMangoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
